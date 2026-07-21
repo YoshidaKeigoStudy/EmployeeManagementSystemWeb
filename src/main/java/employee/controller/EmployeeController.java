3c8c62b18d7b2ee532ceadb6bf41e7db6785651d
K.Yoshida <yoshida@cosmo-w.co.jp>
2026-07-22 02:12:39 +0900

    一覧表示実装途中

--- src/main/java/employee/controller/EmployeeController.java
+++ src/main/java/employee/controller/EmployeeController.java
@@ -1,12 +1,27 @@
 package employee.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import employee.model.Employee;
+import employee.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
-	@GetMapping("/")
-	public String index() {
-		return "index";
+	private EmployeeService employeeService;
+
+	// コンストラクタインジェクション
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@GetMapping("/employees")
+	public String show(Model model) {
+		List<Employee> employees = employeeService.findActiveEmployees();
+		model.addAttribute("employees", employees);
+		return "employeeList";
 	}
 }
