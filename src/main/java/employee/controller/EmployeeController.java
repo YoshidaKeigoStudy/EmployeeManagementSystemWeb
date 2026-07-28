9db93d2d4459b0bc59a2c6f3fe847cc006081cde
K.Yoshida <yoshida@cosmo-w.co.jp>
2026-07-29 02:28:58 +0900

    検索機能実装途中

--- src/main/java/employee/controller/EmployeeController.java
+++ src/main/java/employee/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package employee.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import employee.form.EmployeeForm;
 import employee.model.Employee;
@@ -20,6 +22,7 @@ public class EmployeeController {
 		this.employeeService = employeeService;
 	}
 
+	// 社員一覧表示メソッド
 	@GetMapping("/employees")
 	public String show(Model model) {
 		List<Employee> employees = employeeService.findActiveEmployees();
@@ -27,12 +30,7 @@ public class EmployeeController {
 		return "employeeList";
 	}
 
-	@GetMapping("/regist")
-	public String regist(Model model) {
-
-		return "regist";
-	}
-
+	// 登録メソッド
 	@PostMapping("/regist")
 	public String regist(EmployeeForm employeeForm) {
 		Employee emp = new Employee(employeeForm.getEmployeeId(), employeeForm.getName(), employeeForm.getAge(),
@@ -40,4 +38,13 @@ public class EmployeeController {
 		employeeService.register(emp);
 		return "regist";
 	}
+
+	// 検索メソッド
+	@GetMapping("/employees/search")
+	public String search(@RequestParam("employeeId") String employeeId, Model model) {
+		List<Employee> employees = Arrays.asList(employeeService.search(employeeId));
+		model.addAttribute("employees", employees);
+		return "employeeList";
+	}
+
 }
